c93ecb1e8f4d20da94a5a449ae4385a8807714da
Aleksey Savchenkov <savchenkov.alex@nixsolutions.com>
2014-11-26 14:27:06 +0200

    Test config change

--- config/autoload/local.test.php
+++ config/autoload/local.test.php
@@ -10,13 +10,16 @@ return array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
                 'params' => array(
-                    'host'     => 'alexfloppy-ubnt.php.nixsolutions.com',
+                    'host'     => 'alpha-team.php.nixsolutions.com',
                     'port'     => '3306',
-//                    'user'     => 'root',
-//                    'password' => '',
-                    'dbname'   => 'zf2_starter_alpha_test',
-                    'username' => 'developer',
-                    'password' => '123456',
+                    'dbname'   => 'p_zfs_test',
+                    'username' => 'p_zfs_tests',
+                    'password' => 'p_zfs_test',
+//                    'host'     => 'alexfloppy-ubnt.php.nixsolutions.com',
+//                    'port'     => '3306',
+//                    'dbname'   => 'zf2_starter_alpha_test',
+//                    'username' => 'developer',
+//                    'password' => '123456',
                 ),
                 'doctrine_type_mappings' => array(
                     'enum' => 'string'
