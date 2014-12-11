ac3e7b69f6fcc97333a61651cb9522b197d94587
kazimirchuk <kazimirchuk.alex@nixsolutions.com>
2014-12-11 18:15:33 +0200

    Added some files for audio uploads

---
+++ module/Media/src/Media/Service/Audio.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexander
+ * Date: 12/11/14
+ * Time: 5:44 PM
+ */
+
+namespace Media\Service;
+
+use Zend\Filter\File\RenameUpload;
+use Media\Entity\ObjectImage;
+
+class Audio
+{
+    const PATH = "/uploads/audio/";
+    const PUBLIC_PATH = "public";
+    const GETPATH = true;
+
+    protected $sm;
+
+    public function __construct($sm)
+    {
+        $this->sm = $sm;
+    }
+
+    /**
+     * @param $data
+     * @return \Media\Entity\Audio
+     */
+    public function createAudio($data, $object)
+    {
+        //Creating new image to get ID for building its path
+        $image = new \Media\Entity\Audio();
+        $this->sm->get('doctrine.entitymanager.orm_default')->persist($image);
+        $this->sm->get('doctrine.entitymanager.orm_default')->flush();
+        //Building path and creating directory. Then - moving
+        $ext = \Media\Service\Audio::getExt($data['image']['name']);
+        $destination = \Media\Service\Audio::imgPath(\Media\Service\Audio::ORIGINAL, $image->getId(), $ext);
+        \Media\Service\Audio::moveImage($destination, $data['image']);
+        $image->setExtension($ext);
+        $this->sm->get('doctrine.entitymanager.orm_default')->persist($image);
+
+        $objectImage = new ObjectImage();
+        $objectImage->setImage($image);
+        $objectImage->setEntityName($object->getEntityName());
+        $objectImage->setObjectId($object->getId());
+        $this->sm->get('doctrine.entitymanager.orm_default')->persist($objectImage);
+        $this->sm->get('doctrine.entitymanager.orm_default')->flush();
+
+        return $image;
+    }
+
+    /**
+     * @param $imageId
+     */
+    public function deleteImage($imageId)
+    {
+        $objectImage = $this->sm->get('doctrine.entitymanager.orm_default')->getRepository('Media\Entity\ObjectImage')->findOneByImageId($imageId);
+        $this->sm->get('doctrine.entitymanager.orm_default')->remove($objectImage);
+        $this->sm->get('doctrine.entitymanager.orm_default')->flush();
+
+        $image = $this->sm->get('doctrine.entitymanager.orm_default')->getRepository('Media\Entity\Image')->findOneById($imageId);
+        $this->sm->get('doctrine.entitymanager.orm_default')->remove($image);
+        $this->sm->get('doctrine.entitymanager.orm_default')->flush();
+    }
+
+    /**
+     * @param $destination
+     * @param $image
+     * @return array|string
+     */
+    public static function moveImage($destination, $image)
+    {
+        self::prepareDir($destination);
+        $filter = new RenameUpload(
+            array(
+                "target" => $destination,
+                'randomize' => false,
+            )
+        );
+
+        return $filter->filter($image);
+    }
+
+    /**
+     * @param $path
+     * @param int $mode
+     * @return bool
+     * @throws \Exception
+     */
+    public static function prepareDir($path, $mode = 0775)
+    {
+        $destination = preg_replace('/.[0-9]*\.((jpeg)|(jpg)|(png))$/', '', $path);
+        if (!is_dir($destination)) {
+            if (self::prepareDir(dirname($destination), $mode)) {
+                if (!is_writable(dirname($destination))) {
+                    throw new \Exception('Directory ' . dirname($destination) . 'is not writable');
+                }
+                return mkdir($destination) && chmod($destination, $mode);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $type
+     * @param $id
+     * @param $ext
+     * @param bool $onlyPath
+     * @return string
+     * @throws \Exception
+     */
+    public static function imgPath($type, $id, $ext, $onlyPath = false)//$onlyPath it's because we need another path when working with Original and when we are getting it
+    {
+        if (self::ORIGINAL == $type) {
+            if ($onlyPath == false) {
+                $path = self::PUBLIC_PATH . self::PATH . "original";
+            } else {
+                $path = self::PATH . "original";
+            }
+
+
+        } else {
+            $size = self::sizeByType($type);
+            if (empty($size)) {
+                throw new \Exception('Unsupported size');
+            }
+            $path = self::PATH . $size['width'] . 'x' . $size['height'];
+        }
+
+        return self::buildImagePath($id, $path, $ext);
+    }
+
+    /**
+     * @param $id
+     * @param $path
+     * @param $ext
+     * @return string
+     */
+    public static function buildImagePath($id, $path, $ext)
+    {
+        return rtrim($path, "/") . "/" . trim(self::buildPath($id, $ext), '/');
+    }
+
+    /**
+     * @param $id
+     * @param $ext
+     * @return string
+     */
+    public static function buildPath($id, $ext)
+    {
+        $path = sprintf('%012d', $id);
+        $explodedPath = str_split($path, 3);
+        $temp = implode('/', $explodedPath);
+        $finalPath = $temp . '.' . $ext;
+
+        return $finalPath;
+    }
+
+    /**
+     * @param $imageName
+     * @return mixed
+     */
+    public static function getExt($imageName)
+    {
+        return preg_replace('(.*\.)', '', $imageName);
+    }
+
+    /**
+     * @param $type
+     * @return array
+     */
+    protected static function sizeByType($type)
+    {
+        switch ($type) {
+            case self::BIG_THUMB:
+                return array('width' => self::B_THUMB_WIDTH, 'height' => self::B_THUMB_HEIGHT);
+            case self::SMALL_THUMB:
+                return array('width' => self::S_THUMB_WIDTH, 'height' => self::S_THUMB_HEIGHT);
+        }
+    }
+
+    /**
+     * @param $urlPart
+     * @return string
+     */
+    public function getFullUrl($urlPart)
+    {
+        return $this->sm->get('ViewHelperManager')->get('ServerUrl')->__invoke() . $urlPart;
+    }
+
+    public function generateImageUploadForm($module)
+    {
+        echo $this->sm->get('ViewHelperManager')->get('Partial')->__invoke('layout/file-upload/image-upload-form.phtml');
+        echo "<script>require(['" . $module . "']);</script>";
+    }
+}
---
+++ module/Test/src/Test/Controller/AudioController.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexander
+ * Date: 12/3/14
+ * Time: 11:09 AM
+ */
+
+namespace Test\Controller;
+
+use Media\Service\Image;
+use Zend\View\Model\JsonModel;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Media\Form\ImageUpload;
+use Media\Form\Filter\ImageUploadInputFilter;
+use Media\Interfce\ImageUploaderInterface;
+
+class ImageController extends AbstractActionController implements ImageUploaderInterface
+{
+    public function indexAction()
+    {
+        $entityManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+
+
+        $userEntityRepository = $entityManager->getRepository('User\Entity\User');
+        $user = $userEntityRepository->findOneById($this->identity()->getUser()->getId());
+        $images = $user->getImages();
+
+        return new ViewModel(['images' => $images]);
+    }
+
+    public function uploadImageAction()
+    {
+        $form = new ImageUpload('upload-image');
+        $imageService = new Image($this->getServiceLocator());
+        return new ViewModel(['form' => $form, 'imageService' => $imageService]);
+    }
+
+    /**
+     * Advanced avatar uploader Blueimp UI
+     */
+    public function startUploadAction()
+    {
+
+        $user = $this->identity()->getUser();
+        $imageService = $this->getServiceLocator()->get('Media\Service\Image');
+        $blueimpService = $this->getServiceLocator()->get('Media\Service\Blueimp');
+        if ($this->getRequest()->isPost()) {
+            $form = new ImageUpload('upload-image');
+            $inputFilter = new ImageUploadInputFilter();
+            $form->setInputFilter($inputFilter->getInputFilter());
+
+            $request = $this->getRequest();
+            $post = array_merge_recursive(
+                $request->getPost()->toArray(),
+                $request->getFiles()->toArray()
+            );
+            $this->getServiceLocator()->get('Doctrine\ORM\EntityManager')->getConnection()->beginTransaction();
+            $form->setData($post);
+
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $image = $imageService->createImage($data, $this->identity()->getUser());
+                $this->getServiceLocator()->get('Doctrine\ORM\EntityManager')->getConnection()->commit();
+                $dataForJson = $blueimpService->displayUploadedImage($image, $this->getDeleteUrl($image));
+            } else {
+                $messages = $form->getMessages();
+                $messages = array_shift($messages);
+                $this->getServiceLocator()->get('Doctrine\ORM\EntityManager')->getConnection()->rollBack();
+                $this->getServiceLocator()->get('Doctrine\ORM\EntityManager')->close();
+
+                $dataForJson = [ 'files' => [
+                        [
+                            'name' => $form->get('image')->getValue()['name'],
+                            'error' => array_shift($messages)
+                        ]
+                ]];
+            }
+        } else {
+            $dataForJson = $blueimpService->displayUploadedImages(
+                $user->getImages(),
+                $this->getDeleteUrls($user->getImages())
+            );
+        }
+
+        return new JsonModel($dataForJson);
+    }
+
+    public function deleteImageAction()
+    {
+        $this->getServiceLocator()->get('Media\Service\Image')
+            ->deleteImage($this->getEvent()->getRouteMatch()->getParam('id'));
+        return $this->getServiceLocator()->get('Media\Service\Blueimp')
+            ->deleteImageJson($this->getEvent()->getRouteMatch()->getParam('id'));
+    }
+
+    public function getDeleteUrl($image)
+    {
+        $url = $this->serviceLocator->get('ViewHelperManager')->get('url');
+        $imageService = $this->getServiceLocator()->get('Media\Service\Image');
+        return $imageService->getFullUrl($url('test/default', [
+            'controller' => 'image',
+            'action' => 'delete',
+            'id' => $image->getId()
+        ]));
+    }
+
+    public function getDeleteUrls($images)
+    {
+        $deleteUrls = [];
+        foreach ($images as $image) {
+            array_push($deleteUrls, [
+                'id' => $image->getId(),
+                'deleteUrl' => $this->getDeleteUrl($image)
+            ]);
+        }
+
+        return $deleteUrls;
+    }
+}
