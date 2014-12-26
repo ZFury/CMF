d0ab7279ca46f50f8a4bf84b186c8f5b759daa71
Ravenholm <aedakftw@gmail.com>
2014-12-26 15:21:28 +0200

    Minor fixes

--- module/Test/src/Test/Grid/Grid.php
+++ module/Test/src/Test/Grid/Grid.php
@@ -11,8 +11,7 @@ class Grid extends AbstractGrid
         $em = $this->sm->get('Doctrine\ORM\EntityManager');
         $source = $em->createQueryBuilder()->select(['test.id', 'test.email', 'test.name'])
             ->from('\Test\Entity\Test', 'test');
-        $this->setSource($source)->setEntityAlias('test')
-            ->setColumns(['id' => 'id', 'Email' => 'email', 'Imya' => 'name'])
+        $this->setSource($source)->setColumns(['id' => 'id', 'Email' => 'email', 'Name' => 'name'])
             ->setAllowedFilters(['email', 'name'])->setAllowedOrders(['id', 'name']);
     }
 }
