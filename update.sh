7b72140c147a27207c5359ff45689d8059444929
Aleksey Savchenkov <savchenkov.alex@nixsolutions.com>
2014-12-29 16:23:02 +0200

    Migrations to install.sh/update.sh

--- update.sh
+++ update.sh
@@ -9,5 +9,8 @@ php composer.phar install
 echo "Doctrine setup"
 ./vendor/bin/doctrine-module orm:schema-tool:update --force
 
+echo "Update migrations"
+vendor/doctrine/doctrine-module/bin/doctrine-module migrations:migrate --dry-run
+
 mkdir -p public/uploads
 chmod -R 0777 public/uploads
