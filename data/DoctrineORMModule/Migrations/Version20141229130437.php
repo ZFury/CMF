b84d63e333b5b5980c864ea7c73faa596dcd8c1a
Aleksey Savchenkov <savchenkov.alex@nixsolutions.com>
2014-12-29 12:43:35 +0200

    Migrations for mail_templates moduele (signup,forgot-password)

--- data/DoctrineORMModule/Migrations/Version20141229130437.php
+++ data/DoctrineORMModule/Migrations/Version20141229130437.php
@@ -8,12 +8,13 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20141219143429 extends AbstractMigration
+class Version20141229130437 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("insert  into `mail_templates`(`id`,`alias`,`description`,`subject`,`fromEmail`,`fromName`,`bodyHtml`,`bodyText`,`created`,`updated`) values (1,'sign-up','Template for registration','Congratulation','ZFStarter@admins.com','Admin ZFStarter','<p>Please confirm your registration <a href=\"%confirm%\">confirm</a></p>','Please confirm your registration  %confirm%','2014-12-16 13:04:59','2014-12-17 22:29:46');");
+        $this->addSql("INSERT INTO `mail_templates`(`id`,`alias`,`description`,`subject`,`fromEmail`,`fromName`,`bodyHtml`,`bodyText`,`created`,`updated`)
+            VALUES (null,'sign-up','Template for registration','Congratulation','ZFStarter@admins.com','Admin ZFStarter','<p>Please confirm your registration <a href=\"%confirm%\">confirm</a></p>','Please confirm your registration  %confirm%','2014-12-16 13:04:59','2014-12-17 22:29:46');");
     }
 
     public function down(Schema $schema)
