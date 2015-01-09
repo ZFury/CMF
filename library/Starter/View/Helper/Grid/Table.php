f1c86994c13936c081a69c8f0a62a97f656ac8cb
Ravenholm <aedakftw@gmail.com>
2015-01-09 15:42:27 +0200

    Fixed urls

--- library/Starter/View/Helper/Grid/Table.php
+++ library/Starter/View/Helper/Grid/Table.php
@@ -4,6 +4,10 @@ namespace Starter\View\Helper\Grid;
 
 class Table extends AbstractGridHelper
 {
+    protected $editUrl = 'javascript:;';
+
+    protected $deleteUrl = 'javascript:;';
+
     /**
      * @inheritdoc
      */
@@ -27,14 +31,26 @@ class Table extends AbstractGridHelper
             foreach ($row as $key => $value) {
                 $result .= '<td>' . ($value instanceof \DateTime ? $value->format('Y-m-d H:i:s') : $value) . '</td>';
             }
-            $result .= '<td><a class="btn btn-primary btn-xs" href="javascript:;">';
+            $result .= '<td><a class="btn btn-primary btn-xs" href="' . $this->editUrl . '">';
             $result .= '<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>';
-            $result .= '<a class="btn btn-danger btn-xs" href="javascript:;">';
+            $result .= '<a class="btn btn-danger btn-xs" href="' . $this->deleteUrl .'">';
             $result .= '<span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>';
             $result .= '</td></tr>';
         }
         $result .= '</tbody></table>';
 
         return $result;
+    }
+
+    public function setEditUrl($editUrl)
+    {
+        $this->editUrl = $editUrl;
+
+        return $this;
+    }
+
+    public function setDeleteUrl($deleteUrl)
+    {
+        $this->deleteUrl = $deleteUrl;
     }
 }
