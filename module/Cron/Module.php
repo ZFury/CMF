c78b9b3ecf4e416818b818761cdf53edf6c262ee
Aleksey Savchenkov <savchenkov.alex@nixsolutions.com>
2015-01-16 12:47:54 +0200

    Cron module

---
+++ module/Cron/Module.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Cron;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Class Module
+ * @package Cron
+ */
+class Module
+{
+    /**
+     * @param MvcEvent $e
+     */
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getServiceConfig()
+    {
+        return array();
+    }
+}
---
+++ module/Cron/src/Cron/Controller/IndexController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Cron\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Doctrine\ORM\EntityNotFoundException;
+
+/**
+ * Class CronController
+ * @package Cron\Controller
+ */
+class IndexController extends AbstractActionController
+{
+    /**
+     * @return array|ViewModel
+     * @throws EntityNotFoundException
+     */
+    public function indexAction()
+    {
+        die('cron index');
+    }
+
+    public function testAction()
+    {
+        die('cron test');
+    }
+}
