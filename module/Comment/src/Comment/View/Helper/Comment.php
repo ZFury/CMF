f26be1f1445fffeaec6c7b46e39a2482de06044a
Oleksii Novikov <hunter@nixsolutions.com>
2015-02-12 18:24:06 +0200

    Changed comment helper to use partial template by default

--- module/Comment/src/Comment/View/Helper/Comment.php
+++ module/Comment/src/Comment/View/Helper/Comment.php
@@ -5,21 +5,40 @@ namespace Comment\View\Helper;
 use Zend\View\Helper\HelperInterface;
 use Zend\View\Renderer\RendererInterface as Renderer;
 
+/**
+ * Class Comment
+ * @package Comment\View\Helper
+ */
 class Comment implements HelperInterface
 {
+    /**
+     * @var Renderer|null
+     */
     private $view;
 
+    /**
+     * @param Renderer $view
+     * @return void|HelperInterface
+     */
     public function setView(Renderer $view)
     {
         $this->view = $view;
     }
 
+    /**
+     * @return Renderer
+     */
     public function getView()
     {
         return $this->view;
     }
 
-    public function __invoke($partial, $comments)
+    /**
+     * @param $comments
+     * @param string $partial
+     * @return mixed
+     */
+    public function __invoke($comments, $partial = 'comment/index/partial/comment.phtml')
     {
         return $this->view->partialLoop($partial, $comments);
     }
