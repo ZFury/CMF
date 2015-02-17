5971046b905cfdbba3c037fc0ebd3556386836d7
kazimirchuk <kazimirchuk.alex@nixsolutions.com>
2015-02-17 16:14:49 +0200

    Turned off phpcs and phpcb to test phpunit

--- travis.config.sh
+++ travis.config.sh
@@ -23,7 +23,7 @@ sed -i "s/{PASSWORD}//g" config/autoload/doctrine.local.php
 sed -i "s/{PORT}/3306/g" config/autoload/doctrine.local.php
 
 sed -i "s/{HOSTNAME}/127.0.0.1/g" config/autoload/doctrine.testing.php
-sed -i "s/{DATABASE}/p_zfury/g" config/autoload/doctrine.testing.php
+sed -i "s/{DATABASE}/p_zfury_test/g" config/autoload/doctrine.testing.php
 sed -i "s/{USERNAME}/travis/g" config/autoload/doctrine.testing.php
 sed -i "s/{PASSWORD}//g" config/autoload/doctrine.testing.php
 sed -i "s/{PORT}/3306/g" config/autoload/doctrine.testing.php
