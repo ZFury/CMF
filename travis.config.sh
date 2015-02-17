77acf2e9c2e8a96f66fe75f2bd53188ffbcf19ee
kazimirchuk <kazimirchuk.alex@nixsolutions.com>
2015-02-17 15:14:09 +0200

    Added travis

---
+++ travis.config.sh
@@ -0,0 +1,34 @@
+# Creating directories
+mkdir logs
+mkdir logs/phpcb
+mkdir coverage/options
+mkdir coverage/pages
+mkdir coverage/categories
+mkdir coverage/comment
+mkdir coverage/media
+mkdir coverage/user
+mkdir coverage/install
+
+# Copying dist files
+copy config/autoload/doctrine.local.php.dist config/autoload/doctrine.local.php
+copy config/autoload/doctrine.testing.php.dist config/autoload/doctrine.testing.php
+copy config/autoload/testing.php.dist config/autoload/testing.php
+# copy public/.htaccess.sample public/.htaccess
+
+# Configuring
+sed -i "s/{HOSTNAME}/127.0.0.1/g" config/autoload/doctrine.local.php
+sed -i "s/{DATABASE}/p_zfury/g" config/autoload/doctrine.local.php
+sed -i "s/{USERNAME}/travis/g" config/autoload/doctrine.local.php
+sed -i "s/{PASSWORD}//g" config/autoload/doctrine.local.php
+sed -i "s/{PORT}/3306/g" config/autoload/doctrine.local.php
+
+sed -i "s/{HOSTNAME}/127.0.0.1/g" config/autoload/doctrine.testing.php
+sed -i "s/{DATABASE}/p_zfury/g" config/autoload/doctrine.testing.php
+sed -i "s/{USERNAME}/travis/g" config/autoload/doctrine.testing.php
+sed -i "s/{PASSWORD}//g" config/autoload/doctrine.testing.php
+sed -i "s/{PORT}/3306/g" config/autoload/doctrine.testing.php
+
+sed -i "s/\/\/'BjyAuthorize'/'BjyAuthorize'/g" config/application.config.php
+
+
+
---
+++ travis.phpunit.sh
@@ -0,0 +1,8 @@
+php -r 'putenv('APP_ENV=testing');'
+./vendor/bin/phpunit -c module/Options/test/phpunit.xml --log-junit 'logs/tests-report.options.xml' --coverage-clover 'logs/phpunit.clover.options.xml' --coverage-html 'logs/coverage/options' > logs/phpunit.options.log
+./vendor/bin/phpunit -c module/Pages/test/phpunit.xml --log-junit 'logs/tests-report.pages.xml' --coverage-clover 'logs/phpunit.clover.pages.xml' --coverage-html 'logs/coverage/pages' > logs/phpunit.pages.log
+./vendor/bin/phpunit -c module/Categories/test/phpunit.xml --log-junit 'logs/tests-report.categories.xml' --coverage-clover 'logs/phpunit.clover.categories.xml' --coverage-html 'logs/coverage/categories' > logs/phpunit.categories.log
+./vendor/bin/phpunit -c module/Media/test/phpunit.xml --log-junit 'logs/tests-report.media.xml' --coverage-clover 'logs/phpunit.clover.media.xml' --coverage-html 'logs/coverage/media' > logs/phpunit.media.log
+./vendor/bin/phpunit -c module/User/test/phpunit.xml --log-junit 'logs/tests-report.user.xml' --coverage-clover 'logs/phpunit.clover.user.xml' --coverage-html 'logs/coverage/user' > logs/phpunit.user.log
+./vendor/bin/phpunit -c module/Install/test/phpunit.xml --log-junit 'logs/tests-report.install.xml' --coverage-clover 'logs/phpunit.clover.install.xml' --coverage-html 'logs/coverage/install' > logs/phpunit.install.log
+./vendor/bin/phpunit -c module/Comment/test/phpunit.xml --log-junit 'logs/tests-report.comment.xml' --coverage-clover 'logs/phpunit.clover.comment.xml' --coverage-html 'logs/coverage/comment' > logs/phpunit.comment.log
