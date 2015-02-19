26119029f0fee9f86a232d1aa9cdbae8859cf752
kazimirchuk <kazimirchuk.alex@nixsolutions.com>
2015-02-19 15:59:41 +0200

    Edited travis.config.sh

--- travis.config.sh
+++ travis.config.sh
@@ -10,19 +10,12 @@ mkdir coverage/user
 mkdir coverage/install
 
 # Copying dist files
-copy config/autoload/doctrine.local.php.dist config/autoload/doctrine.local.php
 copy config/autoload/doctrine.testing.php.dist config/autoload/doctrine.testing.php
 copy config/autoload/testing.php.dist config/autoload/testing.php
 # copy public/.htaccess.sample public/.htaccess
 
 # Configuring
-sed -i "s/{HOSTNAME}/127.0.0.1/g" config/autoload/doctrine.local.php
-sed -i "s/{DATABASE}/p_zfury/g" config/autoload/doctrine.local.php
-sed -i "s/{USERNAME}/travis/g" config/autoload/doctrine.local.php
-sed -i "s/{PASSWORD}//g" config/autoload/doctrine.local.php
-sed -i "s/{PORT}/3306/g" config/autoload/doctrine.local.php
-
-sed -i "s/{HOSTNAME}/127.0.0.1/g" config/autoload/doctrine.testing.php
+sed -i "s/{HOSTNAME}/localhost/g" config/autoload/doctrine.testing.php
 sed -i "s/{DATABASE}/p_zfury_test/g" config/autoload/doctrine.testing.php
 sed -i "s/{USERNAME}/travis/g" config/autoload/doctrine.testing.php
 sed -i "s/{PASSWORD}//g" config/autoload/doctrine.testing.php
